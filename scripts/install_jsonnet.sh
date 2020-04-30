d20ba40cf61bc774d1d806785edf065748845b26
Paul Rudin <paul@rudin.co.uk>
2020-04-30 17:40:15 +0100

    don't use sudo in jsonnet install

--- scripts/install_jsonnet.sh
+++ scripts/install_jsonnet.sh
@@ -3,4 +3,4 @@ cd /tmp
 wget https://github.com/google/jsonnet/releases/download/v0.15.0/jsonnet-bin-v0.15.0-linux.tar.gz
 tar xzf jsonnet-bin-v0.15.0-linux.tar.gz
 rm jsonnet-bin-v0.15.0-linux.tar.gz
-sudo install -m 755 jsonnet /usr/local/bin/jsonnet
+install -m 755 jsonnet /usr/local/bin/jsonnet
